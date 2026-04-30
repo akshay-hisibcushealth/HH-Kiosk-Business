f9c74013410c57a07a471034c1aaa5a64c116f88
Akshay <akshay@hibiscushealth.com>
2026-05-01 00:47:39 +0530

    Start app on home screen instead of client ID entry

--- HH_Kiosk_B2B/App/MyApp.swift
+++ HH_Kiosk_B2B/App/MyApp.swift
@@ -19,48 +19,23 @@ struct MyApp: App {
 
 struct RootView: View {
     @StateObject private var appState = AppState()
-    @State private var clientID = LocalUserStorage.loadClientID()
 
     var body: some View {
         ZStack {
-            if clientID == nil {
-                ClientIDEntryScreen { savedClientID in
-                    clientID = savedClientID
-                }
-                .environmentObject(appState)
-                .transition(.opacity)
-            } else if appState.isBrandingLoading {
-                ProgressView()
-                    .progressViewStyle(CircularProgressViewStyle())
-                    .scaleEffect(1.4)
-                    .tint(.white)
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .background(Color(AppColors.primary).ignoresSafeArea())
+            if appState.showScreenSaver {
+                ScreenSaver()
+                    .environmentObject(appState)
                     .transition(.opacity)
+                    .zIndex(1)
             } else {
-                if appState.showScreenSaver {
-                    ScreenSaver()
-                        .environmentObject(appState)
-                        .transition(.opacity)
-                        .zIndex(1)
-                } else {
 //             ResultsViewWrapper()
-                    HomeScreen()
-                        .environmentObject(appState)
-                        .transition(.opacity)
-                        .zIndex(0)
-                }
+                HomeScreen()
+                    .environmentObject(appState)
+                    .transition(.opacity)
+                    .zIndex(0)
             }
         }
         .animation(.easeInOut(duration: 0.5), value: appState.showScreenSaver)
-        .animation(.easeInOut(duration: 0.3), value: clientID == nil)
-        .task(id: clientID) {
-            if let clientID {
-                await appState.loadBrandingData(for: clientID)
-            } else {
-                appState.clearBrandingData()
-            }
-        }
     }
 }
 
