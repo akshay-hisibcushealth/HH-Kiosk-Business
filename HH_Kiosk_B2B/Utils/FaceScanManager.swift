59b7ba79a518b6091197c230913356ec395f5c12
Akshay <akshay@hibiscushealth.com>
2026-07-21 20:04:15 +0530

    Disable lighting quality constraint for face scans

--- HH_Kiosk_B2B/Utils/FaceScanManager.swift
+++ HH_Kiosk_B2B/Utils/FaceScanManager.swift
@@ -143,7 +143,6 @@ class FaceScanManager: ObservableObject{
     func presentAnuraMeasurementViewController(sdkConfig: Data) {
         let measurementConfig = MeasurementConfiguration.defaultConfiguration
         measurementConfig.studyFile = sdkConfig
-        measurementConfig.lightingQualityConstraintEnabled = true
         measurementConfig.externalCameraPreset = cameraPreset
         measurementConfig.externalCameraPreviewOrientation = previewOrientation
         measurementConfig.isExternalCameraVideoMirrored = mirrorExternalCameraPreview
